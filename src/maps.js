fcc5410b1f205ee8178faaf23d11fe4a5a8874f3
razh <razh@users.noreply.github.com>
2021-08-26 00:51:18 -0400

    Add player controls

--- src/maps.js
+++ src/maps.js
@@ -5,7 +5,24 @@ import { keys_create } from './keys.js';
 import { material_create } from './material.js';
 import { mesh_create } from './mesh.js';
 import { object3d_add, object3d_create } from './object3d.js';
-import { vec3_create, vec3_set } from './vec3.js';
+import {
+  BODY_DYNAMIC,
+  BODY_STATIC,
+  get_physics_component,
+  physics_add,
+  physics_bodies,
+  physics_update,
+} from './physics.js';
+import { player_create, player_update } from './player.js';
+import {
+  vec3_applyQuaternion,
+  vec3_create,
+  vec3_cross,
+  vec3_multiplyScalar,
+  vec3_normalize,
+  vec3_set,
+  vec3_setScalar,
+} from './vec3.js';
 
 var keys = keys_create();
 
@@ -31,7 +48,20 @@ export var map0 = (gl, scene, camera) => {
   object3d_add(cameraObject, camera);
   object3d_add(map, cameraObject);
 
-  vec3_set(camera.position, 16, 16, 32);
+  // Action
+  var playerMesh = physics_add(
+    mesh_create(boxGeom_create(30, 56, 30), material_create()),
+    BODY_DYNAMIC,
+  );
+  playerMesh.position.y += 28;
+  playerMesh.visible = false;
+  Object.assign(cameraObject.position, playerMesh.position);
+  object3d_add(map, playerMesh);
+
+  var playerPhysics = get_physics_component(playerMesh);
+  playerPhysics.update = () => {};
+  var player = player_create(playerMesh, playerPhysics);
+  player.scene = map;
 
   var mesh = mesh_create(boxGeom_create(8, 8, 8), material_create());
   mesh.position.y = 4;
@@ -52,7 +82,36 @@ export var map0 = (gl, scene, camera) => {
 
   entity_add(
     map,
-    component_create(() => {}),
+    component_create((component, dt) => {
+      var bodies = physics_bodies(map);
+      physics_update(bodies);
+      player.dt = dt;
+
+      vec3_setScalar(player.command, 0);
+
+      if (keys.KeyW || keys.ArrowUp) player.command.z++;
+      if (keys.KeyS || keys.ArrowDown) player.command.z--;
+      if (keys.KeyA || keys.ArrowLeft) player.command.x--;
+      if (keys.KeyD || keys.ArrowRight) player.command.x++;
+      if (keys.Space) player.command.y++;
+
+      var movespeed = 127;
+      vec3_multiplyScalar(player.command, movespeed);
+
+      vec3_set(player.viewForward, 0, 0, -1);
+      vec3_set(player.viewRight, 1, 0, 0);
+
+      vec3_applyQuaternion(
+        vec3_set(player.viewForward, 0, 0, -1),
+        camera.quaternion,
+      );
+      vec3_normalize(
+        vec3_cross(vec3_set(player.viewRight, 0, -1, 0), player.viewForward),
+      );
+
+      player_update(player);
+      Object.assign(cameraObject.position, playerMesh.position);
+    }),
   );
 
   return {
