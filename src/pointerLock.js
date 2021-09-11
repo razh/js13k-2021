bbf9759df42f1c330791f3c485db00bdf2ada144
razh <razh@users.noreply.github.com>
2021-09-11 16:20:32 -0400

    Remove `document.pointerLockElement` check
    Assume the Pointer Lock API is always supported.

--- src/pointerLock.js
+++ src/pointerLock.js
@@ -1,14 +1,8 @@
 export var pointerLock_create = (controls, element) => {
-  var hasPointerLock = 'pointerLockElement' in document;
-
-  if (!hasPointerLock) {
-    controls.enabled = true;
-    return;
-  }
-
-  document.addEventListener('pointerlockchange', () => {
-    controls.enabled = element === document.pointerLockElement;
-  });
+  document.addEventListener(
+    'pointerlockchange',
+    () => (controls.enabled = element === document.pointerLockElement),
+  );
 
   addEventListener('click', () => element.requestPointerLock());
 };
