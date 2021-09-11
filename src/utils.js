ef4284da5d3aceee9c6ed50704defd4b017dd285
razh <razh@users.noreply.github.com>
2021-09-11 16:24:53 -0400

    Simplify `flow()` utility
    Replace `Array#reduceRight()` with `reduce()`.

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,11 @@
-export var flow = (...fns) =>
-  fns.reduceRight(
-    (f, g) => (...args) => f(g(...args)),
-    _ => _,
-  );
+export var flow = (...fns) => initialValue =>
+  fns.reduce((value, fn) => fn(value), initialValue);
 
 export var rearg = fn => (...args) => value => fn(value, ...args);
 
 export var remove = (array, element) => {
   var index = array.indexOf(element);
-  if (index >= 0) {
-    array.splice(index, 1);
-  }
+  if (index >= 0) array.splice(index, 1);
 };
 
 export var sample = array => array[(Math.random() * array.length) | 0];
